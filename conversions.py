ff6623577012a9808eaf57ebba55fe05390da6ed
rmanzano-sps <reynaldo.manzano@spsmail.cuny.edu>
2018-10-07 00:43:38 -0500

    work in progress

---
+++ conversions.py
@@ -0,0 +1,11 @@
+import numpy
+
+def convertCelsiusToKelvin(celsius):
+    notice = 'Hi from convertCelsiusToKelvin'
+    print(notice)
+    return 0.0
+
+def convertCelsiusToFahrenheit(celsius):
+    notice = 'Hi from convertCelsiusToFahrenheit'
+    frank = -459.67
+    return frank
---
+++ tests.py
@@ -0,0 +1,41 @@
+import unittest
+from conversions import convertCelsiusToKelvin, convertCelsiusToFahrenheit
+import numpy
+
+
+class TemperatireConversionTest(unittest.TestCase):
+
+    temperature_comparison = []
+    counter = 0
+
+    for celsius in numpy.arange(-270.00,501.00, 10.00):
+        temperature_comparison.append([celsius])
+
+    for fahrenheit in numpy.arange(-454.00,933.00, 18.00):
+        temperature_comparison[counter].append(fahrenheit)
+        counter = counter + 1
+
+    counter = 0
+
+    for kelvin in numpy.arange(3.15,774.15, 10):
+        temperature_comparison[counter].append(kelvin)
+        counter = counter + 1
+
+    temperature_comparison.insert(0, [-273.15, -459.67, 0.00])
+
+
+    def test_convertCelsiusToKelvin(self):
+        for temp in self.temperature_comparison:
+            result = convertCelsiusToKelvin(temp[0])
+            print(temp[1], 'here')
+            self.assertEqual(temp[1], result)
+
+    def test_convertCelsiusToFahrenheit(self):
+        for temp in self.temperature_comparison:
+            result = convertCelsiusToFahrenheit(temp[0])
+            print(temp[1], 'here')
+            self.assertEqual(temp[1], result)
+
+
+if __name__ == '__main__':
+    unittest.main()
